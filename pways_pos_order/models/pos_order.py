82212c01ba40e1cecafbf1a56e05eaa25ae5ed67
Dhaval Chavda <precisedhaval@gmail.com>
2024-06-25 15:24:46 +0530

    changes header and added menu creation

--- pways_pos_order/models/pos_order.py
+++ pways_pos_order/models/pos_order.py
@@ -19,6 +19,7 @@ class PosResCompany(models.Model):
     order_delivery_url = fields.Char(string="Get Delivery Agent URL")
     order_pickup_url = fields.Char(string="Order Picked-up URL")
     get_customer_url = fields.Char(string="Get Customer Number URL")
+    menu_creation_url = fields.Char(string="Menu Creation URL")
 
 class ProductCategory(models.Model):
     _inherit = 'product.category'
@@ -35,6 +36,41 @@ class ProductTemplate(models.Model):
     cgst = fields.Float(string="CGST Tax")
     igst = fields.Float(string="IGST Tax")
     sgst = fields.Float(string="SGST Tax")
+    addons_ids = fields.One2many('addon.group','addon_group_id')
+    gst_liability = fields.Char(string="GST Liability")
+    inclusive = fields.Boolean(string="Inclusive")
+    packing_charges = fields.Float(string="Packing Charge")
+    enable = fields.Boolean(string="Enable")
+    addon_free_limit = fields.Integer(string='Addon Free Limit')
+    addon_limit = fields.Integer(string='Addon Limit',)
+    image_url = fields.Char(string='Image URL')
+    image_url_swiggy = fields.Char(string='Swiggy Image URL')
+    image_url_zomato = fields.Char(string='Zomato Image URL')
+    is_goods = fields.Boolean(string='Is Goods')
+    order = fields.Integer(string="Order")
+
+class PosAddonGroup(models.Model):
+    _name = 'addon.group'
+
+    addon_group_id = fields.Many2one('product.template')
+    name = fields.Char(string="Name")
+    addon_min_limit = fields.Char(string="Addon Min Limit")
+    addon_limit = fields.Char(string="Addon Limit")
+    addon_free_limit = fields.Char(string="Addon Free Limit")
+    # addons_product_ids = fields.One2many('addon.group.product','addon_product_id')
+    product_ids = fields.Many2many("product.template")
+
+
+
+# class PosAddonGroup(models.Model):
+#     _name = 'addon.group.product'
+#     _rec_name = 'product_id'
+
+#     addon_product_id = fields.Many2one('addon.group')
+#     product_ids = fields.Many2many("product.template")
+    # is_veg = fields.Boolean(string="Is Veg")
+    # in_stock = fields.Boolean(string="In Stock")
+    # is_default = fields.Boolean(string="Is Default")
 
 class PosSession(models.Model):
     _inherit = 'pos.session'
@@ -120,75 +156,112 @@ class PosOrder(models.Model):
     time_to_arrive = fields.Integer(string="Time to arrive")
 
     def pos_menu_creation(self):
-        pos_category_ids = self.env['product.category'].sudo().search([('parent_id','=',False)])
-        print("pos_category------------------------",pos_category_ids)
-        pos_sub_category_ids = self.env['product.category'].sudo().search([('parent_id','in',pos_category_ids.ids)])
-        print("pos sub category-----------------",pos_sub_category_ids)
-        pos_product_ids = self.env['product.product'].sudo().search([('categ_id','in', pos_sub_category_ids.ids)])
-        print("pos pos_product pos_product-----------------",pos_product_ids)
-        main_categories = self.env['product.category'].sudo().search([('parent_id', '=', False)])
-        category_structure = {"main_categories": []}
+        category_structure = {
+            "merchant_id": "2544",
+            "menu": {
+                "entity": {
+                    "main_categories": [],
+                    "items": []
+                }
+            }
+        }
 
-        for main_category in main_categories:
-            # Fetch sub-categories for each main category
-            sub_categories = self.env['product.category'].sudo().search([('parent_id', '=', main_category.id)])
-            sub_category_list = []
-            product_list = []
-            product_list_without_sub = self.env['product.product'].sudo().search([('categ_id','=', main_category.id)])
-            print("-----------------product_list_without_sub--------------------------",product_list_without_sub)
-            for rec in product_list_without_sub:
-                tax_list = []
-                tax_list.append({
-                    'cgst':rec.cgst,
-                    'igst':rec.igst,
-                    'sgst':rec.sgst
-                })
-                product_list.append({
-                    "id": rec.id,
-                    "name": rec.name,
-                    "category_id": main_category.id ,
-                    "price": rec.list_price,
-                    'is_veg': rec.is_veg,
-                    'in_stock': rec.in_stock,
-                    "description": rec.description,
-                    "gst_details": tax_list
-                }) 
-            for sub_category in sub_categories:
-                # Fetch products for each sub-category
-                products = self.env['product.product'].sudo().search([('categ_id', '=', sub_category.id)])
-                    
-                for product in products:
-                    tax_list = []
-                    tax_list.append({
-                        'cgst':product.cgst,
-                        'igst':product.igst,
-                        'sgst':product.sgst
-                    })
-                    product_list.append({
-                        "id": product.id,
-                        "name": product.name,
-                        "category_id": main_category.id ,
-                        "sub_category_id": sub_category.id,
-                        "price": product.list_price,
-                        'is_veg': product.is_veg,
-                        'in_stock': product.in_stock,
-                        "description": product.description,
-                        "gst_details": tax_list
-                    })                
-                sub_category_list.append({
-                    "id": sub_category.id,
-                    "name": sub_category.name,
-                    "description": sub_category.description,
-                    # "products": product_list
-                })
-            
-            category_structure["main_categories"].append({
-                "id": main_category.id,
-                "name": main_category.name,
-                "sub_categories": sub_category_list,
-                'items': product_list
+        # Fetch all POS categories
+        pos_categories = self.env['product.category'].sudo().search([])
+
+        # Dictionaries to track categories
+        categories_dict = {}
+        main_categories_set = set()
+
+        # Populate categories_dict with initial data
+        for category in pos_categories:
+            categories_dict[category.id] = {
+                "id": category.id,
+                "name": category.name,
+                "description": category.description or "",
+                "order": None,
+                "sub_categories": []
+            }
+            if not category.parent_id:
+                main_categories_set.add(category.id)
+
+        # Populate sub_categories
+        for category in pos_categories:
+            if category.parent_id:
+                parent_id = category.parent_id.id
+                if parent_id in categories_dict:
+                    categories_dict[parent_id]["sub_categories"].append(categories_dict[category.id])
+                    # Remove subcategory from main_categories_set if it is part of a subcategory
+                    if category.id in main_categories_set:
+                        main_categories_set.remove(category.id)
+
+        # Function to remove empty sub_categories
+        def remove_empty_sub_categories(category):
+            if not category["sub_categories"]:
+                category.pop("sub_categories")
+            else:
+                for sub_category in category["sub_categories"]:
+                    remove_empty_sub_categories(sub_category)
+
+        # Append main categories and subcategories to main_categories
+        for main_category_id in main_categories_set:
+            main_category = categories_dict[main_category_id]
+            remove_empty_sub_categories(main_category)
+            category_structure["menu"]["entity"]["main_categories"].append(main_category)
+
+        # Fetch all products
+        pos_products = self.env['product.product'].sudo().search([('enable', '=', True)])
+
+        # Populate items
+        for product in pos_products:
+            if product.categ_id.parent_id:
+                main_categorie_id = product.categ_id.parent_id.id
+                sub_category_id = product.categ_id.id
+            else:
+                main_categorie_id = product.categ_id.id
+                sub_category_id = None
+            category_structure["menu"]["entity"]["items"].append({
+                "id": product.id,  # Ensure id is a string
+                "category_id": str(main_categorie_id),  # Ensure category_id is a string
+                "sub_category_id": str(main_categorie_id),
+                "name": product.name,
+                "is_veg": product.is_veg,
+                "description": product.description or "",
+                "price": str(product.list_price),  # Ensure price is a string
+                "gst_details": None,
+                "packing_charges": str(product.packing_charges) or "0",  # Ensure packing_charges is a string
+                "enable": 1 if product.enable else 0,
+                "in_stock": 1 if product.in_stock else 0,
+                "addon_free_limit": -1,
+                "addon_limit": -1,
+                "image_url": product.image_url or "",
+                "item_slots": [],
+                "image_url_swiggy": product.image_url_swiggy or "",
+                "image_url_zomato": product.image_url_zomato or "",
+                "is_goods": False,
+                "variant_groups": [],
+                "addon_groups": [],
+                "pricing_combinations": [],
+                "order": 2,
+                "recommended": False,
+                "catalog_attributes": {
+                    "spice_level": None,
+                    "sweet_level": None,
+                    "gravy_property": None,
+                    "bone_property": None,
+                    "contain_seasonal_ingredients": None,
+                    "accompaniments": None,
+                    "quantity": None,
+                    "serves_how_many": None
+                }
             })
-        print("category_structure000-------------------",category_structure)
+        headers = {"X-Wera-Api-Key": "8cab0be2-1972-480d-a077-5f5a905806dc", "Content-Type": "application/json","Accept": "application/json"}
+        url = self.company_id.menu_creation_url
+        if not url or url == False:
+            raise ValidationError(_('"Insert Menu Creation URL in Company."'))
+        response = requests.post(url=url, json=category_structure, headers=headers)
+        print("response------------------",response)
+        return category_structure
 
     def action_auto_accept(self):
         today = datetime.now()
@@ -205,7 +278,7 @@ class PosOrder(models.Model):
         url = self.company_id.accept_url
         print("url----------------------",url)
         data = {'merchand_id': self.restaurant_id ,'order_id': self.order_id}
-        headers = {"charset": "utf-8", "Content-Type": "application/json"}
+        headers = {"X-Wera-Api-Key": "8cab0be2-1972-480d-a077-5f5a905806dc", "Content-Type": "application/json","Accept": "application/json"}
         if not url or url == False:
             raise ValidationError(_('"Insert Order Accept URL in Company."'))
         response = requests.post(url=url, json=data, headers=headers)
