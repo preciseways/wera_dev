a0e5a2d728012123321119d9df69bc3305d14470
Dhaval Chavda <precisedhaval@gmail.com>
2024-07-26 12:45:36 +0530

    product cateogry and help message

--- pways_pos_order/models/inherit_product.py
+++ pways_pos_order/models/inherit_product.py
@@ -8,22 +8,22 @@ from odoo import http, _
 class PosConfig(models.Model):
     _inherit = "pos.config"
 
-    online_order = fields.Boolean(string="Online Order")
+    online_order = fields.Boolean(string="Online Order", help="Select the This to create Online Platform Order")
 
 class PosResCompany(models.Model):
     _inherit = "res.company"
 
-    accept_url = fields.Char(string="Order Accept URL")
-    reject_url = fields.Char(string="Order Reject URL")
-    food_ready_url = fields.Char(string="Food Ready URL")
-    call_support = fields.Char(string="Call Swiggy Partner URL")
-    order_delivery_url = fields.Char(string="Get Delivery Agent URL")
-    order_pickup_url = fields.Char(string="Order Picked-up URL")
-    get_customer_url = fields.Char(string="Get Customer Number URL")
-    menu_creation_url = fields.Char(string="Menu Creation URL")
-    order_reject_webhook = fields.Char(string="Odoo Reject Webhook URL", compute='create_config_reject_url')
-    order_place_order_webhook = fields.Char(string="Odoo Place Order Webhook URL", compute='create_config_place_order_url')
-    order_push_delivery_agent_webhook = fields.Char(string="Odoo Push Delivery Agent Webhook URL", compute='create_config_push_delivery_url')
+    accept_url = fields.Char(string="Order Accept URL", help="This URL is used to post the status of order accepted")
+    reject_url = fields.Char(string="Order Reject URL",help="This URL is used to post the status of order rejected")
+    food_ready_url = fields.Char(string="Food Ready URL",help="This URL is used to post the status of order is ready")
+    call_support = fields.Char(string="Call Swiggy Partner URL",help="This URL is used to call the swiggy Parnter")
+    order_delivery_url = fields.Char(string="Get Delivery Agent URL",help="This URL is used to get delivery Agent")
+    order_pickup_url = fields.Char(string="Order Picked-up URL", help="This URL is used to post the status of order Picked-up by Delivery Agent or Customer")
+    get_customer_url = fields.Char(string="Get Customer Number URL",help="This URL is used to get customer Contact Number")
+    menu_creation_url = fields.Char(string="Menu Creation URL", help="This URL is used to post the order in wera")
+    order_reject_webhook = fields.Char(string="Odoo Reject Webhook URL", compute='create_config_reject_url', help="This URL is for the Rejecting Order from the Wera Side")
+    order_place_order_webhook = fields.Char(string="Odoo Place Order Webhook URL", compute='create_config_place_order_url',help="This URL Is for the Place Order From the Wera Side")
+    order_push_delivery_agent_webhook = fields.Char(string="Odoo Push Delivery Agent Webhook URL", compute='create_config_push_delivery_url',help="This URL used for the wera side to push delivery agent")
 
     def create_config_reject_url(self):
         base_url = http.request.env['ir.config_parameter'].get_param('web.base.url')
@@ -37,41 +37,46 @@ class PosResCompany(models.Model):
         base_url = http.request.env['ir.config_parameter'].get_param('web.base.url')
         self.order_push_delivery_agent_webhook = base_url+'/order/push/delivery-agent'
 
-class ProductCategory(models.Model):
-    _inherit = 'product.category'
+# class ProductCategory(models.Model):
+#     _inherit = 'product.category'
 
-    description = fields.Char(string="Description", help="Insert the Category Description")
+#     description = fields.Char(string="Description", help="Insert the Category Description")
+
+class PosCategory(models.Model):
+    _inherit = 'pos.category'
+
+    pos_category_description = fields.Char(string="Description", help="Insert the Category Description")
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     wera_id = fields.Integer(string="Wera Item ID", help="Insert Wera Item ID")
-    is_veg = fields.Boolean(string="Is Veg")
-    in_stock = fields.Boolean(string="In Stock")
-    description = fields.Char(string="Description")
+    is_veg = fields.Boolean(string="Is Veg", help="Is Veg")
+    in_stock = fields.Boolean(string="In Stock", help="In Stock")
+    description = fields.Char(string="Description", help="Insert Food Description")
     cgst = fields.Float(string="CGST Tax")
     igst = fields.Float(string="IGST Tax")
     sgst = fields.Float(string="SGST Tax")
     addons_group_ids = fields.Many2many('addon.group')
     gst_liability = fields.Char(string="GST Liability", help="Insert the Name who is responsible for GST")
     inclusive = fields.Boolean(string="Inclusive")
-    packing_charges = fields.Float(string="Packing Charge")
-    enable = fields.Boolean(string="Enable")
+    packing_charges = fields.Float(string="Packing Charge", help="Insert Packing Charge")
+    enable = fields.Boolean(string="Enable", help="Is Enable")
     addon_free_limit = fields.Integer(string='Addon Free Limit', help="Insert the Addon Free Limit Which you want to give the customer free addon")
     addon_min_limit = fields.Integer(string="Addon Min Limit", help="Insert the Addon Min Limit whih you want to limit the minimun addon of the product")
     addon_limit = fields.Integer(string='Addon Limit',help="Insert the Max allowed addons")
-    image_url = fields.Char(string='Image URL')
-    image_url_swiggy = fields.Char(string='Swiggy Image URL')
-    image_url_zomato = fields.Char(string='Zomato Image URL')
+    image_url = fields.Char(string='Image URL', help="Insert Food Image URL")
+    image_url_swiggy = fields.Char(string='Swiggy Image URL', help="Insert Food Image URL For Swiggy")
+    image_url_zomato = fields.Char(string='Zomato Image URL', help="Insert Food Image URL For Zomato")
     is_goods = fields.Boolean(string='Is Goods')
     order = fields.Integer(string="Order")
-    preparation_time = fields.Integer(string="Preparation Time")
+    preparation_time = fields.Integer(string="Preparation Time" ,help="Insert Preparation Time Of Food")
     slot_ids = fields.One2many('item.slots','slot_id')
     company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.company, ondelete='cascade')
 
 
     def fetch_categories(self):
-        pos_categories = self.env['product.category'].sudo().search([])
+        pos_categories = self.env['pos.category'].sudo().search([])
 
         categories_dict = {}
         main_categories_set = set()
@@ -130,7 +135,7 @@ class ProductTemplate(models.Model):
             "cgst": cgst,
             "sgst": sgst,
             "inclusive": False,
-            "gst_liability": "SWIGGY"
+            "gst_liability": False
         } if taxes else None
 
         return gst
@@ -303,21 +308,21 @@ class PosProductItemSlots(models.Model):
     _name = 'item.slots'
 
     slot_id = fields.Many2one('product.template')
-    start_hour = fields.Float(string="Open Time")    
-    end_hour = fields.Float(string="Close Time")
+    start_hour = fields.Float(string="Open Time" , help="Open Time")    
+    end_hour = fields.Float(string="Close Time", help="Close Time")
     week_ids = fields.Many2many('week.day')
 
 class Weekday(models.Model):
     _name = 'week.day'
     _description = 'Week Day'
 
-    name = fields.Char(string="Day Name", required=True)
+    name = fields.Char(string="Day Name", required=True, help="Week Day")
 
 class PosAddonGroup(models.Model):
     _name = 'addon.group'
 
     addon_group_id = fields.Many2one('product.template')
-    name = fields.Char(string="Name")
+    name = fields.Char(string="Name" ,help="Addon Group Name")
     addon_free_limit = fields.Integer(string='Addon Free Limit', help="Insert the Addon Free Limit Which you want to give the customer free addon")
     addon_min_limit = fields.Integer(string="Addon Min Limit", help="Insert the Addon Min Limit whih you want to limit the minimun addon of the product")
     addon_limit = fields.Integer(string='Addon Limit',help="Insert the Max allowed addons")
@@ -329,10 +334,10 @@ class PosAddonGroup(models.Model):
     _name = 'addon.group.product'
 
     addon_product_id = fields.Many2one('addon.group')
-    product_name = fields.Char(string="Product Name")
-    is_veg = fields.Boolean(string="Is Veg")
-    in_stock = fields.Boolean(string="In Stock")
-    is_default = fields.Boolean(string="Is Default")
+    product_name = fields.Char(string="Product Name" , help="Insert Product Name")
+    is_veg = fields.Boolean(string="Is Veg", help="Is Veg")
+    in_stock = fields.Boolean(string="In Stock", help="In Stock")
+    is_default = fields.Boolean(string="Is Default", help="Is Default")
     order = fields.Integer(string="order")
-    price = fields.Float(string="Price")
+    price = fields.Float(string="Price", help="Insert the Price of Product")
     taxes_id = fields.Many2many('account.tax')
